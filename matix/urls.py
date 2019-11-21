92f57a6f7813fb943fad731b063b887cfa856e43
大巴0007 <yld060631@163.com>
2019-11-21 15:18:36 +0800

    fix: urls

--- matix/urls.py
+++ matix/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from matix import views
 
 urlpatterns = [
+    url(r'^$', views.index),
     url(r'^index/$', views.index),
     url(r'^contact_us/$', views.contact_us),
     url(r'^get_board/$', views.get_board),
